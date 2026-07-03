5e7076c9ae1fa49343567b9ef866aac41c5958bd
1VicTim1 <122aaa121@gmail.com>
2026-07-03 11:22:03 +0500

    build: Enable ccache for Kleaf wrappers

--- build_abi.sh
+++ build_abi.sh
@@ -6,6 +6,33 @@ set -e
 DEVICE_MODULES_DIR=$(basename $(dirname $0))
 source "${DEVICE_MODULES_DIR}/kernel/kleaf/_setup_env.sh"
 
+CCACHE_EXEC=${CCACHE_EXEC:-$(command -v ccache || true)}
+CCACHE_DIR=${CCACHE_DIR:-${HOME}/.cache/ccache/inferno-kernel}
+CCACHE_MAXSIZE=${CCACHE_MAXSIZE:-35G}
+CCACHE_CPP2=${CCACHE_CPP2:-yes}
+CCACHE_NOHASHDIR=${CCACHE_NOHASHDIR:-true}
+KLEAF_CCACHE_WRAPPER_DIR=${KLEAF_CCACHE_WRAPPER_DIR:-${CCACHE_DIR}/wrappers}
+
+KLEAF_CCACHE_ARGS=()
+if [[ -n "${CCACHE_EXEC}" ]]
+then
+  mkdir -p "${CCACHE_DIR}"
+  mkdir -p "${KLEAF_CCACHE_WRAPPER_DIR}"
+  ln -sf "${CCACHE_EXEC}" "${KLEAF_CCACHE_WRAPPER_DIR}/clang"
+  ln -sf "${CCACHE_EXEC}" "${KLEAF_CCACHE_WRAPPER_DIR}/clang++"
+  "${CCACHE_EXEC}" -M "${CCACHE_MAXSIZE}" >/dev/null
+  export CCACHE_EXEC CCACHE_DIR CCACHE_MAXSIZE CCACHE_CPP2 CCACHE_NOHASHDIR KLEAF_CCACHE_WRAPPER_DIR
+  KLEAF_CCACHE_ARGS=(
+	"--action_env=CCACHE_EXEC=${CCACHE_EXEC}"
+	"--action_env=CCACHE_DIR=${CCACHE_DIR}"
+	"--action_env=CCACHE_MAXSIZE=${CCACHE_MAXSIZE}"
+	"--action_env=CCACHE_CPP2=${CCACHE_CPP2}"
+	"--action_env=CCACHE_NOHASHDIR=${CCACHE_NOHASHDIR}"
+	"--action_env=KLEAF_CCACHE_WRAPPER_DIR=${KLEAF_CCACHE_WRAPPER_DIR}"
+	"--sandbox_writable_path=${CCACHE_DIR}"
+  )
+fi
+
 # run kleaf commands or legacy build.sh
 result=$(echo ${KLEAF_SUPPORTED_PROJECTS} | grep -wo ${PROJECT}) || result=""
 if [[ ${result} != "" ]]
@@ -18,7 +45,7 @@ then
 fi
 
 KLEAF_OUT=("--output_user_root=${OUT_DIR} --output_base=${OUT_DIR}/bazel/output_user_root/output_base")
-KLEAF_ARGS=("${DEBUG_ARGS} ${SANDBOX_ARGS} --experimental_writable_outputs --noenable_bzlmod")
+KLEAF_ARGS=("${DEBUG_ARGS} ${SANDBOX_ARGS} ${KLEAF_CCACHE_ARGS[*]} --experimental_writable_outputs --noenable_bzlmod")
 
 set -x
 (
